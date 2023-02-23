a377dcd13a10c8a4516fca6d53705f2abb725317
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2023-02-24 00:31:10 +0200

    non-existing row mapping removed

--- src/main/java/com/falcontech/batchpostgresmysql/processor/ItemProcessor.java
+++ src/main/java/com/falcontech/batchpostgresmysql/processor/ItemProcessor.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class ItemProcessor implements org.springframework.batch.item.ItemProcessor<Student, com.falcontech.batchpostgresmysql.mysql.entity.Student> {
 
   @Override
-  public com.falcontech.batchpostgresmysql.mysql.entity.Student process(com.falcontech.batchpostgresmysql.postgres.entity.Student item) throws Exception {
+  public com.falcontech.batchpostgresmysql.mysql.entity.Student process(com.falcontech.batchpostgresmysql.postgres.entity.Student item) {
 
     System.out.println(item.getId());
 
@@ -18,9 +18,6 @@ public class ItemProcessor implements org.springframework.batch.item.ItemProcess
     student.setFirstName(item.getFirstName());
     student.setLastName(item.getLastName());
     student.setEmail(item.getEmail());
-    student.setDeptId(item.getDeptId());
-    student.setIsActive(item.getIsActive() != null ?
-        Boolean.valueOf(item.getIsActive()) : false);
 
     return student;
 
