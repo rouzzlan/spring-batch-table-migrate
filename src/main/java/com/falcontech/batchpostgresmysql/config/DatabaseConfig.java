7af2349409413ca7f5e30a0ac330a26b24155f52
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2023-02-24 15:50:48 +0200

    driver config modified

--- src/main/java/com/falcontech/batchpostgresmysql/config/DatabaseConfig.java
+++ src/main/java/com/falcontech/batchpostgresmysql/config/DatabaseConfig.java
@@ -2,10 +2,10 @@ package com.falcontech.batchpostgresmysql.config;
 
 import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -20,19 +20,34 @@ public class DatabaseConfig {
   @Primary
   @ConfigurationProperties(prefix = "spring.datasource")
   public DataSource datasource() {
-    return DataSourceBuilder.create().build();
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+    dataSource.setUrl("jdbc:mysql://localhost:3317/spring_batch");
+    dataSource.setUsername("user");
+    dataSource.setPassword("pass");
+    return dataSource;
+
   }
 
   @Bean
-  @ConfigurationProperties(prefix = "spring.universitydatasource")
   public DataSource universitydatasource() {
-    return DataSourceBuilder.create().build();
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+    dataSource.setUrl("jdbc:mysql://localhost:3316/university");
+    dataSource.setUsername("user");
+    dataSource.setPassword("pass");
+    return dataSource;
   }
 
   @Bean
   @ConfigurationProperties(prefix = "spring.postgresdatasource")
   public DataSource postgresdatasource() {
-    return DataSourceBuilder.create().build();
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName("org.postgresql.Driver");
+    dataSource.setUrl("jdbc:postgresql://localhost:5433/university");
+    dataSource.setUsername("user");
+    dataSource.setPassword("pass");
+    return dataSource;
   }
 
   @Bean
